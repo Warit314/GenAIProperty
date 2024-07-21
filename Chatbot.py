802fb8b1f5d4f5582b9d62647cfc6654cbb7924e
Warit314 <warit@phankrawee.com>
2024-07-22 00:35:37 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Chatbot.py
    	modified:   backend/Prompt.py
    	modified:   start_uvicorn.bat
    
    Update prompt. improve search rate

--- Chatbot.py
+++ Chatbot.py
@@ -3,153 +3,130 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 import json
+import uuid
+
 # Define the API endpoint
 base_api_url = "http://127.0.0.1:8001"
-st.title("💬 Chatbot")
+
+# Title and caption
+st.title("💬 Property Chatbot")
 st.caption("🚀 A Streamlit chatbot powered by Gemma 2b-it")
 st.caption("Source: https://github.com/Warit314/GenAIRecuitment")
 
-# Cache plot map data so it doesn't reload everytime
+# Initialize session state
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "Ask me about property you have selected, or Search through me!"}]
+if "results" not in st.session_state:
+    st.session_state["results"] = []
+if "selected_properties" not in st.session_state:
+    st.session_state["selected_properties"] = []
+
+# Cache plot map data so it doesn't reload every time
 @st.cache_data
 def plot_map(df):
+    st.write("## Property Map (non-interactive)")
     property_layer = pdk.Layer(
         'ScatterplotLayer',
         data=df,
         get_position=['LONGITUDE', 'LATITUDE'],
-        get_radius=100,
-        get_color=[255, 0, 0],
+        get_radius=50,
+        get_color=[0, 0, 0],
         pickable=True,
         auto_highlight=True
     )
-    
-    # Define the tooltip
     tooltip = {
         "html": "<b>{NAME}</b><br>Latitude: {LATITUDE}<br>Longitude: {LONGITUDE}",
         "style": {"backgroundColor": "steelblue", "color": "white"}
     }
-
-    # Define the view state
     view_state = pdk.ViewState(
         latitude=df['LATITUDE'].mean(),
         longitude=df['LONGITUDE'].mean(),
-        zoom=12,
+        zoom=11,
         pitch=50,
     )
-
-    # Create the deck.gl map
     r = pdk.Deck(
         layers=[property_layer],
         initial_view_state=view_state,
-        tooltip=tooltip
+        tooltip=tooltip,
+        map_style=None
     )
-
-    # Display the map in Streamlit
     st.pydeck_chart(r)
 
-def ask_chatbot(prompt,response):
-        chatbot_payload= {
-            "message": prompt,
-            "query_result": json.dumps(response,ensure_ascii=False)
-        }
-        # Send the POST request to the API and handle the streaming response
-        with requests.post(f"{base_api_url}/chat_bot", json=chatbot_payload, stream=True) as response:
-            if response.status_code == 200:
-                st.write_stream(response.iter_content(decode_unicode=True))
-            else:
-                msg = "Sorry, there was an error processing your request."
-                st.session_state.messages.append({"role": "assistant", "content": msg})
-                st.chat_message("assistant").write(msg)
+def ask_chatbot(prompt, response):
+    chatbot_payload = {
+        "message": prompt,
+        "query_result": json.dumps(response, ensure_ascii=False)
+    }
+    with requests.post(f"{base_api_url}/chat_bot", json=chatbot_payload, stream=True) as response:
+        if response.status_code == 200:
+            st.write_stream(response.iter_content(decode_unicode=True))
+        else:
+            msg = "Sorry, there was an error processing your request."
+            st.session_state.messages.append({"role": "assistant", "content": msg})
+            st.chat_message("assistant").write(msg)
 
 def llm_generate_query(prompt):
-    #payload for api requests
-    payload = {
-        "message": prompt
-    }
-
-    #to get widgets
+    payload = {"message": prompt}
     response = requests.post(f"{base_api_url}/generate_query_and_query_sqlite", json=payload, stream=True)
-    
-    #SET STATE
-    st.session_state["results"]=response.json()
+    st.session_state["results"] = response.json()
 
 def display_properties(result):
-    st.write(f"# {result[0].get('NAME')}")#write the name of the condo by select the first one
-    columns = st.columns(len(result))  # Create 3 columns
 
-    for i in range(len(result)):
-
-        if i < len(result):
-            property = result[i]
-            col_index = i % len(result)  # Determine the column to place the property in
-            with columns[col_index].container(border=True):  # Place the property in the appropriate column
-                st.write(f"{property.get('TITLE')}")
-                st.write("**Price:** ", property.get("PRICE"))
+    for i, property in enumerate(result):
+        checkbox_key = f"checkbox_{i}"
+        with st.container(border=True):
+            st.caption(i + 1)
+            st.write(f"{property.get('TITLE')}")
+            st.write("**Price:** ", property.get("PRICE"))
 
 
-#INIT
-# Fetch data from API
-response = requests.get(f"{base_api_url}/Get_map_plot_data")
-
-if response.status_code == 200:
-    map_data = response.json()
-    df = pd.DataFrame(map_data)
-    
-    plot_map(df)
-    
-    
-    # Dropdown for property selection in the sidebar with "Search Through LLM" as default value
-    selected_property = st.sidebar.selectbox(
-        'Select a property to ask AI or Search Through LLM:',
-        df['NAME'],
-    )
-    search_through_llm = st.sidebar.toggle("Search Through LLM")
-    # Display selected property details
-    if selected_property and not search_through_llm:
-        selected_info = df[df['NAME'] == selected_property].iloc[0]
-        
-        payload={
-            "message": f"SELECT name,title,details, price, routed_distance, routed_time, nearest_stations,pool,sauna,fitness,jacuzzi,keycard,laundry,parking,shuttle,allowPet,security,restaurant FROM sample_data WHERE name = '{selected_info['NAME']}' LIMIT 3"
-        }
-        
-        response = requests.post(f"{base_api_url}/Query_Sqlite" ,json=payload)
-
-        st.session_state["results"]=response.json()
-        display_properties(st.session_state["results"])
-else:
-    st.error('Failed to fetch data to plot the map!')
-    print('Failed to fetch data to plot the map!')
-
-
-
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Ask me about any property in Thailand"}]
-    
-if "results" not in st.session_state:
-    st.session_state["results"] = []
-
-if "current_index" not in st.session_state:
-    st.session_state["current_index"] = 0
-
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-
-    #if the user didn't select property from drop down and ask directly
-    if st.session_state["results"] ==None or search_through_llm:
-        query_result=llm_generate_query(prompt)
-
-        if st.session_state["results"]:
-            st.write("Found Properties:")
-            display_properties(st.session_state["results"])
-
-        ask_chatbot(prompt,st.session_state["results"])
-
-
+def fetch_map_data():
+    response = requests.get(f"{base_api_url}/Get_map_plot_data")
+    if response.status_code == 200:
+        map_data = response.json()
+        df = pd.DataFrame(map_data)
+        plot_map(df)
+        return df
     else:
-        display_properties(st.session_state["results"])
-        ask_chatbot(prompt,st.session_state["results"])
+        st.error('Failed to fetch data to plot the map!')
+        return None
+
+def main():
+    df = fetch_map_data()
+    if df is not None:
+        selected_property = st.sidebar.selectbox(
+            'Select a property to ask AI or Search Through LLM:',
+            df['NAME']
+        )
+        search_through_llm = st.sidebar.toggle("Search Through LLM")  # Fixed from toggle to checkbox
+        
+        if selected_property and not search_through_llm:
+            selected_info = df[df['NAME'] == selected_property].iloc[0]
+            payload = {
+                "message": f"SELECT name,title,details, price, routed_distance, routed_time, nearest_stations,pool,sauna,fitness,jacuzzi,keycard,laundry,parking,shuttle,allowPet,security,restaurant FROM sample_data WHERE name = '{selected_info['NAME']}' LIMIT 3"
+            }
+            response = requests.post(f"{base_api_url}/Query_Sqlite", json=payload)
+            st.session_state["results"] = response.json()
+            display_properties(st.session_state["results"])
+        
+        # Display messages after properties are displayed
+        for msg in st.session_state.messages:
+            st.chat_message(msg["role"]).write(msg["content"])
+
+        # Handle chat input
+        if prompt := st.chat_input():
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.chat_message("user").write(prompt)
+            
+            if st.session_state["results"] == [] or search_through_llm:
+                llm_generate_query(prompt)
+                if st.session_state["results"]:
+                    st.caption("Found Properties:")
+                    display_properties(st.session_state["results"])
+                ask_chatbot(prompt, st.session_state["results"])
+            else:
+                display_properties(st.session_state["results"])
+                ask_chatbot(prompt, st.session_state["results"])
+
+if __name__ == "__main__":
+    main()
--- backend/Prompt.py
+++ backend/Prompt.py
@@ -3,14 +3,15 @@ class Prompt():
     gemini_prompt_answer="""
     <bos><start_of_turn>system
     You are a property expert tasked with helping clients find rental properties in Bangkok, Thailand. 
-    You have access to detailed listings of various properties, including their names, locations, prices, features, and contact information for agents. 
-    Below is property data, leverage this to provide comprehensive recommendations and insights:
+    You have access to detailed listings of various properties, including their names, locations, prices, features (one mean it has the feature and zero mean it doesn't), and contact information for agents. 
+    Below is property data, Your response must include details of the property and contact information if provided.
     <<Property Data>>
     {query_result}
     <<End of Property Data>>
 
     You MUST use only this information to response to the user, DO NOT use other information
-
+    
+    Use heading and subheading for better readability. Answer in human format.
     <end_of_turn>
     <start_of_turn>user
     {message}<end_of_turn>
@@ -18,32 +19,42 @@ class Prompt():
     """
     gemini_prompt_query="""
     <bos><start_of_turn>system
-    You are an intelligent assistant tasked with extracting data for property listings. Below is the data model that will be used to create an SQL query. 
+    You are an intelligent assistant tasked with extracting data for finding property listings. Below is the Schema. 
     Your task is to:
     - Extract data from the user's question.
     - Format the extracted data in JSON, with keys matching the variable names from the given schema.
     <<Schema>>
-    name: Optional[str] - Property name in English (if available)
+    name: Optional[str] - Condo name (if provided)
     price_less_than: Optional[int] - Rental price per month less than the specified amount (if available)
-    routed_distance: Optional[str] - Distance to key locations (if available)
-    routed_time: Optional[str] - Time to key locations (if available)
     nearest_stations: Optional[str] - Nearest public transport stations (if available)
     pool: Optional[bool] - Indicates if the property has a pool (if available)
-    sauna: Optional[bool] - Indicates if the property has a sauna (if available)
     fitness: Optional[bool] - Indicates if the property has a fitness center (if available)
-    jacuzzi: Optional[bool] - Indicates if the property has a jacuzzi (if available)
-    keycard: Optional[bool] - Indicates if the property has keycard access (if available)
-    laundry: Optional[bool] - Indicates if the property has laundry facilities (if available)
     parking: Optional[bool] - Indicates if the property has parking (if available)
     shuttle: Optional[bool] - Indicates if the property has a shuttle service (if available) 
     allowPet: Optional[bool] - Indicates if pets are allowed(if available) 
-    security: Optional[bool] - Indicates if the property has security services (if available)
-    restaurant: Optional[bool] - Indicates if the property has a restaurant (if available)
+
+    
+    <<Example 1 >>
+    user: I want to find condo ABC where the price is less than 15000
+
+    assistant:
+    {{
+        "name" : "ABC",
+        "price" : 15000
+    }} 
+
+    <<Example 2 >>
+    user: I want to find condo with a pool
+
+    assistant:
+    {{
+        "pool" : true
+    }} 
 
     You MUST only return the result in json format.
+
     <end_of_turn>
     <start_of_turn>user
-    <<User question>> 
-    {message}<end_of_turn>
+    user: {message}<end_of_turn>
     <start_of_turn>model
     """
